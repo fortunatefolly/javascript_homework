4b87f12359483426c5e981c79eff9cdc576cdefb
fortunatefolly <williamharry.fortuna@journalism.cuny.edu>
2016-02-15 00:12:48 -0500

    Javascript can add for you.

---
+++ thejavascript.js
@@ -0,0 +1,12 @@
+var a = 2;
+var b = 13;
+
+var added = a + b;
+var subtracted = a - b;
+var multiplied = a * b;
+var divided = a / b;
+
+document.getElementById("c").innerHTML="When I add " +a+ " and " +b+ " I get = " +added;
+document.getElementById("d").innerHTML="When I subtract " +a+ " and " +b+ " I get = " +subtracted;
+document.getElementById("e").innerHTML="When I multiply " +a+ " and " +b+ " I get = " +multiplied;
+document.getElementById("f").innerHTML="When I divide " +a+ " and " +b+ " I get = " +divided;
